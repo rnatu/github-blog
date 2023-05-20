4d32fea3c4b24055abd1d31b775dd4e4fd556e2c
Renato Xavier <rnatu91@gmail.com>
2023-05-20 17:06:13 -0300

    feat: add textSizes on default theme

--- src/styles/default.ts
+++ src/styles/default.ts
@@ -1,15 +1,25 @@
 export const defaultTheme = {
-  'base-input': '#040f1a',
-  'base-background': '#071422',
-  'base-profile': '#0b1b2b',
-  'base-post': '#112131',
-  'base-border': '#1c2f41',
+  colors: {
+    'base-input': '#040f1a',
+    'base-background': '#071422',
+    'base-profile': '#0b1b2b',
+    'base-post': '#112131',
+    'base-border': '#1c2f41',
 
-  'base-label': '#3a536b',
-  'base-span': '#7b96b2',
-  'base-text': '#afc2d4',
-  'base-subtitle': '#c4d4e3',
-  'base-title': '#e7edf4',
+    'base-label': '#3a536b',
+    'base-span': '#7b96b2',
+    'base-text': '#afc2d4',
+    'base-subtitle': '#c4d4e3',
+    'base-title': '#e7edf4',
 
-  'brand-blue': '#3294f8',
+    'brand-blue': '#3294f8',
+  },
+  textSizes: {
+    'title-title-l': '1.5rem',
+    'title-title-m': '1.25rem',
+    'title-title-s': '1.125rem',
+    'text-text-m': '1rem',
+    'text-text-s': '.875rem',
+    'components-link': '.75rem',
+  },
 } as const;
