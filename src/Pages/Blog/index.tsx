0f7dd28c02385456eb08927fa7776fb1ca73f15e
Renato Xavier <rnatu91@gmail.com>
2023-05-25 13:46:05 -0300

    feat: add publications header

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -1,11 +1,22 @@
 import { Header } from '../../components/Header';
+import { Text, Title } from '../../components/Typography';
 import { Profile } from './components/Profile';
+import { PublicationsHeader } from './styles';
 
 export function Blog() {
   return (
     <>
       <Header />
       <Profile />
+
+      <PublicationsHeader>
+        <Title size="s" color="subtitle">
+          Publicações
+        </Title>
+        <Text size="s" color="span">
+          {6} publicações
+        </Text>
+      </PublicationsHeader>
     </>
   );
 }
---
+++ src/Pages/Blog/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const PublicationsHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+
+  max-width: 54rem;
+  margin: 4.6875rem auto 0;
+`;
