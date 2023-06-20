b8fa6f1effafccb2182a1458f5bc6f2d6add1f0c
Renato Xavier <rnatu91@gmail.com>
2023-06-20 16:14:39 -0300

    feat(Blog): get post with axios using query params

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Text, Title } from '../../components/Typography';
 import { Post } from './components/Post';
 import { Profile } from './components/Profile';
 import { SearchInput } from './components/SearchInput';
 import { BlogContainer, PostsContainer, PublicationsHeader } from './styles';
+import { api } from '../../lib/axios';
+
+interface PostData {
+  id: number;
+  repository_url: string;
+  title: string;
+  body: string;
+}
 
 export function Blog() {
   const githubUserName = 'rnatu';
+  const [postData, setPostData] = useState<PostData[]>([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const query = `user:${githubUserName} repo:github-blog`;
+
+      const { data } = await api.get('/search/issues', {
+        params: {
+          q: query,
+        },
+      });
+
+      const filteredIssues: PostData[] = data.items.filter(
+        (issue: PostData) =>
+          issue.repository_url ===
+          'https://api.github.com/repos/rnatu/github-blog',
+      );
+
+      setPostData(filteredIssues);
+    }
+
+    getPosts();
+  }, []);
 
   return (
     <>
@@ -24,12 +56,9 @@ export function Blog() {
         <SearchInput />
 
         <PostsContainer>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          {postData.map((post: PostData) => (
+            <Post key={post.id} />
+          ))}
         </PostsContainer>
       </BlogContainer>
     </>
